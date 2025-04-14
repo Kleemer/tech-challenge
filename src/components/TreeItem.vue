f39475967c525ce321591ae52967b0b2e2f05b22
Chaouki FREJ <chaoukifrej@gmail.com>
2025-04-14 19:42:30 +0200

    feat(component): add recursive TreeItem component for hierarchical display

---
+++ src/components/TreeItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div
+            @click="toggle"
+            :style="{ cursor: hasChildren ? 'pointer' : 'default' }"
+        >
+            <span v-if="hasChildren">{{ isOpen ? '▼' : '▶' }}</span>
+            <span v-else>•</span>
+            {{ item.name }}
+        </div>
+        <div v-if="isOpen" style="padding-left: 1rem">
+            <TreeItem
+                v-for="(child, index) in item.children"
+                :key="index"
+                :item="child"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TreeItem',
+    props: {
+        item: Object,
+    },
+    data() {
+        return {
+            isOpen: true,
+        };
+    },
+    computed: {
+        hasChildren() {
+            return this.item.children && this.item.children.length > 0;
+        },
+    },
+    methods: {
+        toggle() {
+            if (this.hasChildren) {
+                this.isOpen = !this.isOpen;
+            }
+        },
+    },
+};
+</script>
